7bd02024c49859799c4a187c9c114bbecadeb898
xyugen <alex.arias.jorym@gmail.com>
2024-03-26 05:00:25 +0800

    feat: create front page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,13 +19,13 @@ const layout = (
     <html lang='en'>
         <body
             className={cn(
-                'bg-slate-900 font-poppins antialiased',
+                'bg-slate-900 min-h-screen font-poppins antialiased',
                 fontPoppins.variable
                 )}
             >
-            <header className='my-7 mx-16 flex justify-between'>
-                <Link href='/' className='text-gray-200 text-3xl font-bold'>Chatterly</Link>
-                <div>
+            <header className='flex justify-between py-2 px-4 sm:py-7 sm:mx-16'>
+                <Link href='/' className='bg-gradient-to-br from-blue-300 to-blue-500 text-transparent bg-clip-text text-3xl font-bold'>Chatterly</Link>
+                <div className='hidden sm:block sm:visible'>
                     <Link className={buttonVariants({ variant: 'link' })} href='/login'>Login</Link>
                     <Link className={buttonVariants({ variant: 'outline' })} href='/signup'>Sign Up</Link>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <main className='text-white'>Welcome to Chatterly.</main>
+    <main className='text-gray-100 m-8 grid grid-flow-row sm:grid-flow-col sm:m-16'>
+      <div className='flex flex-col justify-center h-66 gap-6 text-center sm:text-left'>
+        <h1 className='font-bold text-3xl sm:text-5xl'>Welcome to <span className='text-blue-500'>Chatterly</span>.</h1>
+        <p className='text-gray-300 text-md'>A free and open-source chat application for the web.</p>
+        
+        <div>
+          <Button asChild variant={'outline'}>
+            <Link href='/signup' className='text-gray-100'>Get Started</Link>
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        <img src='/chatterly.png' alt='Chatterly Logo' className='h-64' />
+      </div>
+    </main>
   )
 }
 
