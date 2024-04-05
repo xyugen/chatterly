5379a377943e1f7c09571143fee5d7a64ac2b136
xyugen <alex.arias.jorym@gmail.com>
2024-04-05 15:08:15 +0800

    feat: add UserForm component & convert Card to Form component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,41 +1,9 @@
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import Link from 'next/link'
-import React from 'react'
+"use client"
+
+import UserForm from '@/components/UserForm'
 
 const SignUpForm = () => {
-  return (
-    <main className='text-gray-100 px-6 md:px-0 mx-auto w-full sm:w-[500px] space-y-12'>
-        <Card>
-            <CardHeader>
-                <CardTitle className='text-4xl bg-gradient-to-br font-bold p-1 from-blue-300 to-blue-500 text-transparent bg-clip-text text-center tracking-wide'>
-                  Sign Up
-                </CardTitle>
-            </CardHeader>
-            <CardContent className='flex flex-col gap-4 mb-3'>
-                <div className='flex flex-col gap-2'>
-                    <Label htmlFor='username'>Username</Label>
-                    <Input id='username' type='text' placeholder='Username' required></Input>
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <Label htmlFor='password'>Password</Label>
-                    <Input id='password' type='password' placeholder='Password' required></Input>
-                </div>
-                <div className='flex flex-col gap-2'>
-                    <Label htmlFor='confirm-password'>Confirm Password</Label>
-                    <Input id='confirm-password' type='password' placeholder='Confirm password' required></Input>
-                </div>
-            </CardContent>
-            <CardFooter className='flex flex-col gap-3'>
-                <Button variant={ 'outline' } className='w-full'>Sign Up</Button>
-                <p>Already have an account? <Link href='/login' className='text-blue-300 hover:underline underline-offset-2'>Sign In</Link></p>
-                {/* <div className='border-t border-slate-700 w-full mt-5 bg-slate-400'></div> */}
-            </CardFooter>
-        </Card>
-    </main>
-  )
+  return UserForm({ type: 'signup' })
 }
 
 export default SignUpForm
